7118fa53f6d1f4219b439e84c1658036cbc72c4c
ansh7520 <ansh7520@colorado.edu>
2024-12-11 17:52:57 -0700

    new stuff

--- FindNISNESS.m
+++ FindNISNESS.m
@@ -30,7 +30,7 @@ function [epsNEESbar,r1x,r2x,epsNISbar,r1y,r2y, NEES, NIS] = FindNISNESS(N,del_x
     plot(epsNEESbar,'ro','MarkerSize',6,'LineWidth',2),hold on
     plot(r1x*ones(size(epsNEESbar)),'r--','LineWidth',2)
     plot(r2x*ones(size(epsNEESbar)),'r--','LineWidth',2)
-    ylabel('NEES statistic, \bar{\epsilon}_x','FontSize',14)
+    ylabel('NEES statistic, $\bar{\epsilon}_x$','FontSize',14,'Interpreter','latex')
     xlabel('time step, k','FontSize',14)
     title('NEES Estimation Results','FontSize',14)
     legend('NEES @ time k', 'r_1 bound', 'r_2 bound')
@@ -46,7 +46,7 @@ function [epsNEESbar,r1x,r2x,epsNISbar,r1y,r2y, NEES, NIS] = FindNISNESS(N,del_x
     plot(epsNISbar,'bo','MarkerSize',6,'LineWidth',2),hold on
     plot(r1y*ones(size(epsNISbar)),'b--','LineWidth',2)
     plot(r2y*ones(size(epsNISbar)),'b--','LineWidth',2)
-    ylabel('NIS statistic, \bar{\epsilon}_y','FontSize',14)
+    ylabel('NIS statistic, $\bar{\epsilon}_y$','FontSize',14,'Interpreter','latex')
     xlabel('time step, k','FontSize',14)
     title('NIS Estimation Results','FontSize',14)
     legend('NIS @ time k', 'r_1 bound', 'r_2 bound')
@@ -64,7 +64,7 @@ subplot(5,1,4)
 plot(innovation(4,:));
 subplot(5,1,5)
 plot(innovation(5,:));
-title('innovation')
+sgtitle('innovation')
 
 
 end
